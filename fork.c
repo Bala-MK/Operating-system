5337fc8db9d70be6756c1a244de383e0ffc9ab2a
Bala-Manikanta <49619628+Bala-Manikanta@users.noreply.github.com>
2019-04-14 23:40:18 +0530

    Add files via upload

---
+++ fork.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<sys/wait.h>
+void sequence(int n){
+	printf("Sequence:\n");
+	while(n>0){
+		printf("%d\n",n);
+        n=n/2;
+	}
+}
+int main(){
+	int n=0;
+	printf("Enter a Positive Integer:");
+	scanf("%d",&n);
+	while(n<0){
+		printf("Positive number required.\nEnter Again:");
+		scanf("%d",&n);
+	}
+	pid_t p;
+	p=fork();
+	if(p==0){
+		printf("Child Process is Executing:\n");
+		printf("Process id=%d\nMy Parent Process id=%d\n",getpid(),getppid());
+		sequence(n);
+
+	}
+	else{
+		wait(NULL);
+		printf("Parent Process is Executing:\n");
+		printf("Process id=%d\nMy Parent Process id=%d\n",getpid(),getppid());
+	}
+}
---
+++ pipe.c
@@ -0,0 +1,23 @@
+#include<fcntl.h>
+#include<stdlib.h>
+#include<stdio.h>
+#include<unistd.h>
+int main(int c,char *v[]){
+	int p[2];
+	char r[100];
+	int v1=open(v[1],O_RDONLY);
+	int v2=open(v[2],O_WRONLY);
+	pipe(p);
+	pid_t a;
+	a=fork();
+	if(a==0){
+		close(p[1]);
+		int s=read(p[0],r,sizeof(r));
+		write(v2,r,s);
+	}
+	else{
+		close(p[0]);
+		int v=read(v1,r,sizeof(r));
+		write(p[1],r,v);
+	}
+}
